33e1efff2c0bfe86c802577f7ef40a1949ae50e2
Mathilde Duverger <mathildeduverger@dyn1193-64.wlan.ic.ac.uk>
2016-12-11 15:38:59 +0000

    Part3 up to 2)

--- part3/part31_dev.f90
+++ part3/part31_dev.f90
@@ -3,10 +3,11 @@ module sync
 	complex(kind=16), parameter :: ii=cmplx(0.0,1.0) !ii = sqrt(-1)
 	integer :: ntotal !total number of nodes
 	real(kind=8) :: c !coupling coefficient
-    real(kind=8), allocatable, dimension(:) :: w !array of frequencies
+        real(kind=8), allocatable, dimension(:) :: w !array of frequencies
 	save
     contains
 
+!f2py -c part31_dev.f90 -m p3 
 
 !---------------------------
 subroutine rk4(t0,y0,dt,nt,y,order)
@@ -26,32 +27,40 @@ subroutine rk4(t0,y0,dt,nt,y,order)
     real(kind=8), dimension(size(y0),nt+1), intent(out) :: y
     real(kind=8), dimension(nt), intent(out) :: order
     real(kind=8), dimension(size(y0)) :: f1, f2, f3, f4
-    real(kind=8) :: t,halfdt,fac
-    integer:: k
+    real(kind=8) :: t,halfdt,fac, sumexp
+    integer:: N,k,j
     
-        halfdt = 0.5d0*dt
-        fac = 1.d0/6.d0
+    N = size(y0)
+    allocate(w(N))
+    
+    halfdt = 0.5d0*dt
+    fac = 1.d0/6.d0
 
-        y(:,1) = y0 !initial condition
-        t = t0 !initial time
+    y(:,1) = y0 !initial condition
+    t = t0 !initial time
 
-        do k = 1, nt !advance nt time steps
+    do k = 1, nt !advance nt time steps
+    
+        f1 = dt*RHS(t, y(:,k))
 
-           f1 = dt*RHS(t, y)
+        f2 = dt*RHS(t + halfdt, y(:,k) + 0.5d0*f1)
 
-           f2 = dt*RHS(t + halfdt, y + 0.5d0*f1)
+        f3 = dt*RHS(t + halfdt, y(:,k) + 0.5d0*f2)
 
-           f3 = dt*RHS(t + halfdt, y + 0.5d0*f2)
+        f4 = dt*RHS(t + dt, y(:,k) + f3)
+        
+        y(:,k+1) = y(:,k) + (f1 + 2*f2  + 2*f3 + f4)*fac
 
-           f4 = dt*RHS(t + dt, y + f3)
-
-           y(:,k+1) = y(:,k) + (f1 + 2*f2  + 2*f3 + f4)*fac
-
-           t = t + dt
+        t = t + dt
         
         !add code to compute order
-           
+        sumexp = 0
+        do j=1,N
+            sumexp = sumexp +  exp(ii*y(j,k+1))
         end do
+        order(k) = abs(sumexp)/N
+        
+    end do
         
         
 end subroutine rk4
@@ -64,10 +73,18 @@ function RHS(t,f)
     real(kind=8), intent(in) :: t
     real(kind=8), dimension(:), intent(in) :: f
     real(kind=8), dimension(size(f)) :: RHS
-    integer :: i1
+    real(kind=8) :: sumsin
+    integer :: N,i,j
 
- !Add code to compute RHS
- 
+    !Add code to compute RHS
+    N = size(f)
+    do i=1,N
+        sumsin = 0
+        do j=1,N
+            sumsin = sumsin + sin(f(i)-f(j))
+        end do
+        RHS(i) = w(i) + c*sumsin/N
+    end do
  
 end function RHS
 !---------------------------
--- part3/part3_dev.py
+++ part3/part3_dev.py
@@ -5,18 +5,24 @@ from p3 import sync #assumes that fortran module sync has been compiled with f2p
 
 
 
-def oscillator(Nt,T,N,c,mu=1,s):
+def oscillator(Nt,T,N,c,mu,s):
     """Simulate fully-coupled network of oscillators
     Compute simulation for Nt time steps between 0 and T (inclusive)
     input: N: number of oscillators
            c: coupling coefficient
            mu,sigma: distribution parameters for omega_i
     """
-    
+    omega = np.random.normal(mu,s,N)
+    theta0  = np.random.uniform(0,2*np.pi,N)
+    dt = T/Nt
+    theta,order = sync.rk4(0,theta0,dt,Nt)
+    t = np.linspace(0,T,Nt)
+    return t,omega,theta0,theta,order
     
     
 
 if __name__ == '__main__':
     n,c,m,s = 101,10.0,1.0,0.1
     Nt,T = 500,100
-    t,omega,theta0,theta,order = kserial(Nt,T,n,c,m,s)
+    t,omega,theta0,theta,order = oscillator(Nt,T,n,c,m,s)
+    print order[0]
